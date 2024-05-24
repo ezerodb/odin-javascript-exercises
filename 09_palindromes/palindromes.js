d331ad4eed6a7582b66847fb6561c4907ae887f0
Ezequiel <ezequielr178@gmail.com>
2024-05-24 09:32:50 -0400

    Solved exercise 09
    Modified palindromes.js to work as intended

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let string1 = "";
+    let string2 = "";
+
+    const regex = /\W/;
+
+    const strArray = string.split("");
+    let newStrArray = []
+
+    strArray.map((char) => {
+        if (!regex.test(char)) {
+            newStrArray.push(char);
+            string1 += char;
+        }
+    })
+
+    for (let i = 0; i < newStrArray.length; i++) {
+        string2 += newStrArray[(newStrArray.length - 1)- i];
+    }
+
+    string1 = string1.toLocaleLowerCase();
+    string2 = string2.toLocaleLowerCase();
+
+    if (string1 === string2) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
